2422fbe1e549233a1b99a65f9f0e99b5029ee58f
kirantr <kirantr@yandex.ru>
2017-08-08 19:39:57 +0300

    2

--- task2/index.php
+++ task2/index.php
@@ -8,8 +8,21 @@ define("DBHOST", "localhost");    // адрес сервера
 $db_server = mysql_connect(DBHOST, DBUSER, DBPASS) or die(mysql_error());
 mysql_select_db(DBNAME) or die(mysql_error());
 
-$query = "INSERT INTO task2('ID', 'name', 'description') VALUE('1', 'name1','description') ;
-mysql_query($query);
+    for($i=0; $i<5; $i++)
+    {
+//        $query = "INSERT INTO task2(ID, name, description) VALUE('$i', 'name$i','description$i')" ;
+  //      mysql_query($query);
+    }
 
+$query2 = "SELECT * FROM task2";
+$result=mysql_query($query2);
+
+while ($row = mysql_fetch_row($result, MYSQL_ASSOC)) 
+    {
+         echo $row['ID']. ' ';
+        echo $row['name']. ' ';
+         echo $row['description'] . '<br>';
+
+     }
 
 ?>
