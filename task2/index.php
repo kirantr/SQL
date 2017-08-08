819538205dca395defa1b4c1524a9d1c7d7fd8a1
kirantr <kirantr@yandex.ru>
2017-08-08 19:47:32 +0300

    Update index.php

--- task2/index.php
+++ task2/index.php
@@ -1,3 +1,15 @@
 <?php
 
+define("DBHOST", "localhost");    // адрес сервера
+        define("DBNAME", "user7");       // имя базы данных
+        define("DBUSER", "user7");   // имя пользователя
+        define("DBPASS", "tuser7");   // пароль доступа к базе данных
+
+$db_server = mysql_connect(DBHOST, DBUSER, DBPASS) or die(mysql_error());
+mysql_select_db(DBNAME) or die(mysql_error());
+
+$query = "INSERT INTO task2('ID', 'name', 'description') VALUE('1', 'name1','description') ;
+mysql_query($query);
+
+
 ?>
